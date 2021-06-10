b84ab1950a962c7c1d7492fe316ac93d0e6629a2
LigetiKaroly <ligeti.karoly78@gmail.com>
2021-06-10 22:10:28 +0200

    Release EmployeeApp v0.0.2:  - Change pom.xml (encoding, dependencies and plugins for Maven),  - Get Employee, EmployeeService and EmployeeServiceTest classes from djteslas gitrepo. Thanx Mate! :)  - Create 3-layer structure (Controller, Service and Repository),  - Create 2 blank class files in the repository and the controller.

---
+++ EmployeeApp/src/main/java/employee/service/EmployeeService.java
@@ -0,0 +1,29 @@
+package employee.service;
+
+import employee.entity.Employee;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EmployeeService {
+
+    private List<Employee> employees = new ArrayList<>();
+
+    public void save(Employee employee) {
+        employees.add(employee);
+    }
+
+    public Employee findOldestEmployee() {
+        return employees.stream().sorted(Comparator.comparing(Employee::getYearOfBirth)).collect(Collectors.toList()).get(0);
+    }
+
+    public List<Employee> findBySalaryLargerThan(int salary) {
+        return employees.stream().filter(e -> e.getSalary() > salary).collect(Collectors.toList());
+    }
+
+    public List<Employee> getEmployees() {
+        return new ArrayList<>(employees);
+    }
+}
---
+++ EmployeeApp/src/test/java/service/EmployeeServiceTest.java
@@ -0,0 +1,45 @@
+package service;
+
+import employee.entity.Employee;
+import employee.service.EmployeeService;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EmployeeServiceTest {
+
+    private EmployeeService services = new EmployeeService();
+
+    @Test
+    void testSaveEmployeeThenList() {
+        services.save(new Employee("John Doe", 1970, 200_000));
+        List<Employee> employees = services.getEmployees();
+
+        assertEquals(1, employees.size());
+        assertEquals("John Doe", employees.get(0).getName());
+    }
+
+    @Test
+    void testFindOldestEmployee() {
+        services.save(new Employee("John Doe", 1970, 200_000));
+        services.save(new Employee("Jane Doe", 1980, 250_000));
+        services.save(new Employee("Jack Doe", 1990, 180_000));
+
+        assertEquals("John Doe", services.findOldestEmployee().getName());
+    }
+
+    @Test
+    void testFindBySalaryLargerThan() {
+        services.save(new Employee("John Doe", 1970, 200_000));
+        services.save(new Employee("Jane Doe", 1980, 250_000));
+        services.save(new Employee("Jack Doe", 1990, 180_000));
+
+        List<Employee> result = services.findBySalaryLargerThan(200_000);
+
+        assertEquals(1, result.size());
+        assertEquals("Jane Doe", result.stream().map(Employee::getName).toList().get(0));
+    }
+
+}
